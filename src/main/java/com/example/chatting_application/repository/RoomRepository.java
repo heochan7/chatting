ccfba80b30ec337959eb387e3eb82a13a829859f
Heochan1125 <geehee1125@gmail.com>
2025-05-09 14:09:00 +0900

    방 리스트 출력하는 query문 실수
    room(채팅방) 기준으로 query 문을 작성하여 수정하게 되었다.
    room_participants(방에 참가한 인원) 기준으로 query 문을 작성했어야 했는데 실수를 하여 다시 수정하였다.

--- src/main/java/com/example/chatting_application/repository/RoomRepository.java
+++ src/main/java/com/example/chatting_application/repository/RoomRepository.java
@@ -16,10 +16,10 @@ public interface RoomRepository extends JpaRepository<Room, Integer> {
             value = "SELECT r.name AS room_name, " +
                     "(SELECT m2.message FROM message m2 WHERE m2.room_id = r.id AND m2.sent_at > rp_me.joined_at ORDER BY m2.sent_at DESC LIMIT 1) AS recent_message, " +
                     "COALESCE((SELECT MAX(m3.sent_at) FROM message m3 WHERE m3.room_id = r.id AND m3.sent_at > rp_me.joined_at), rp_me.joined_at) AS recent_message_time, " +
-                    "(SELECT COUNT(*) FROM room_participants rp2 WHERE rp2.room_id = r.id) AS participant_count " +
-                    "FROM room r " +
-                    "JOIN users u ON u.id = r.manager_user_id " +
-                    "JOIN room_participants rp_me ON rp_me.room_id = r.id AND rp_me.user_id = u.id " +
+                    "(SELECT COUNT(*) FROM room_participants rp2 join users u2 on u2.id = rp2.user_id where u2.email = u.email) AS participant_count " +
+                    "FROM room_participants rp_me " +
+                    "LEFT JOIN room r ON r.id = rp_me.room_id " +
+                    "JOIN users u ON u.id = rp_me.user_id " +
                     "WHERE u.email = :email " +
                     "ORDER BY recent_message_time DESC",
             nativeQuery = true
